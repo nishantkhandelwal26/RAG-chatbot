7aaf95035503af62eb295063d14a498ddfa1875b
nishantkhandelwal26 <nishantkhandelwal111@gmail.com>
2026-05-11 10:58:20 +0530

    remove GROQ_API_KEY and HF_TOKEN

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 os.environ["HF_TOKEN"] = os.getenv("HF_TOKEN")
-os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
 
 
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
